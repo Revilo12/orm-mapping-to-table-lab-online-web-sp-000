e5c9fb7d248687c45088a0925181fa3b4eab8223
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-02 15:57:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
     sql_row = <<-SQL
             SELECT last_insert_rowid()
             SQL
-    id = DB[:conn].execute(sql_row)
+    id = DB[:conn].execute(sql_row).flatten
     @id = id
   end
 
